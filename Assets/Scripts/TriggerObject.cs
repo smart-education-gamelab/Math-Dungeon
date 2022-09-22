500b304e1341002bea1906b1f3c0f1110ba7cd7f
Iris <irisoostra@upcmail.nl>
2022-09-22 13:39:48 +0200

    Shows key to press when inside collider around ball

--- Assets/Scripts/TriggerObject.cs
+++ Assets/Scripts/TriggerObject.cs
@@ -2,6 +2,12 @@ using System.Collections.Generic;
 using UnityEngine;
 public class TriggerObject : MonoBehaviour {
 	[SerializeField] private List<GameObject> objectsToActivate = new List<GameObject>();
+	[SerializeField] private GameObject KeyHintImage;
+
+	private void Start() {
+		//KeyHintImage = GameObject.FindWithTag("Key Hint");
+		//KeyHintImage.SetActive(false);
+	}
 
 	private void Update() {
 		
@@ -10,9 +16,19 @@ public class TriggerObject : MonoBehaviour {
 	private void OnTriggerEnter(Collider other) {
 		if(other.gameObject.tag == "Player") {
 			foreach(GameObject objectToActivate in objectsToActivate) {
-				objectToActivate.GetComponent<InteractableMechanism>().Activate();
+				if(Input.GetButtonDown("Activate")) {
+					objectToActivate.GetComponent<InteractableMechanism>().Activate();
+				}
+
+				KeyHintImage.SetActive(true);
 			}
 			/*Debug.Log("bingo");*/
+		}
+	}
+
+	private void OnTriggerExit(Collider other) {
+		if(other.gameObject.tag == "Player") {
+			KeyHintImage.SetActive(false);
 		}
 	}
 }
