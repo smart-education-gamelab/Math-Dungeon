8f20efbdb21da248b537f93a0653d48cb6875f4c
ikarian7 <scott.marchant7@gmail.com>
2023-07-12 15:06:01 +0200

    Gear pickup and drop
    added gear pick-up and gear dropping

---
+++ Assets/Scripts/PickupObject.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class PickupObject : NetworkBehaviour
+{
+    public Transform objectHolder; // The Transform that represents the position of the object holder
+    public LayerMask pickupLayer; // The layer of the objects that can be picked up
+    public Camera cam;
+
+    [SerializeField]
+    private NetworkObject currentObject; // The current picked-up object
+
+    private void Update()
+    {
+        if (!IsLocalPlayer)
+            return;
+
+        // Check if the player presses the pickup button
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (currentObject == null)
+            {
+                // Raycast to select and pick up an object
+                Debug.DrawRay(cam.transform.position, cam.transform.forward, Color.red, 100f);
+                RaycastHit hit;
+                if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, Mathf.Infinity, pickupLayer))
+                {
+                    // Send an RPC to the server to pick up the object
+                    PickUpObjectServerRpc(hit.transform.gameObject.GetComponent<NetworkObject>().NetworkObjectId);
+                }
+            }
+            else
+            {
+                // Send an RPC to the server to drop the object
+                DropObjectServerRpc();
+            }
+        }
+
+        if (currentObject != null)
+        {
+            // Update the position and rotation of the held object to match the object holder
+            currentObject.transform.position = objectHolder.position;
+            currentObject.transform.rotation = objectHolder.rotation;
+        }
+    }
+
+    [ServerRpc]
+    private void PickUpObjectServerRpc(ulong objectId)
+    {
+        if (currentObject != null)
+            return;
+
+        // Mark the object as picked up
+        currentObject = NetworkManager.Singleton.SpawnManager.SpawnedObjects[objectId];
+        currentObject.GetComponent<Rigidbody>().isKinematic = true;
+
+        // Send an RPC to all clients to synchronize the changes in the picked-up object
+        PickUpObjectClientRpc(currentObject.NetworkObjectId);
+    }
+
+    [ClientRpc]
+    private void PickUpObjectClientRpc(ulong objectId)
+    {
+        // Mark the object as picked up
+        if (NetworkManager.Singleton.SpawnManager.SpawnedObjects.TryGetValue(objectId, out NetworkObject obj))
+        {
+            currentObject = obj;
+            currentObject.GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+
+    [ServerRpc]
+    private void DropObjectServerRpc()
+    {
+        if (currentObject == null)
+            return;
+
+        // Unmark the object as picked up and let it drop
+        currentObject.GetComponent<Rigidbody>().isKinematic = false;
+
+        // Send an RPC to all clients to synchronize the changes in the picked-up object
+        DropObjectClientRpc(currentObject.NetworkObjectId);
+
+        currentObject = null;
+    }
+
+    [ClientRpc]
+    private void DropObjectClientRpc(ulong objectId)
+    {
+        // Unmark the object as picked up and let it drop
+        if (NetworkManager.Singleton.SpawnManager.SpawnedObjects.TryGetValue(objectId, out NetworkObject obj))
+        {
+            obj.GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+}
