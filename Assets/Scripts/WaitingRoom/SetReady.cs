c9e75cc55caeeccc4df3ecd6541a8ec7d65ba0d0
Iris <irisoostra@upcmail.nl>
2023-08-29 17:41:04 +0200

    Correcte spawn locaties

--- Assets/Scripts/WaitingRoom/SetReady.cs
+++ Assets/Scripts/WaitingRoom/SetReady.cs
@@ -54,8 +54,8 @@ public class SetReady : NetworkBehaviour
 
             //uncomment welke je wilt laden
 
-            //Loader.LoadNetwork(Loader.Scene.PuzzleOneDoors);
-            Loader.LoadNetwork(Loader.Scene.PuzzleTwoGears);
+            Loader.LoadNetwork(Loader.Scene.PuzzleOneDoors);
+            //Loader.LoadNetwork(Loader.Scene.PuzzleTwoGears);
         }
     }
 
