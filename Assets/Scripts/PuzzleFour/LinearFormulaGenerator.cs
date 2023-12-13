0a7d65bdae7712a10ef0264c1fccffb4eda2b60c
Iris <i.oostra@ziggo.nl>
2023-12-13 16:19:45 +0100

    Code bijna af

--- Assets/Scripts/PuzzleFour/LinearFormulaGenerator.cs
+++ Assets/Scripts/PuzzleFour/LinearFormulaGenerator.cs
@@ -7,9 +7,28 @@ public class LinearFormulaGenerator : MonoBehaviour {
     [SerializeField]
     private int maxValue = 10;
 
+    Vector2 aAndBValue = new Vector2(99, 99); //Later uit elkaar trekken?
+    string linearFormula = "placeholder";
+
+    Vector2 pointA = new Vector2(99, 99);
+    Vector2 pointB = new Vector2(99, 99);
+    Vector2 pointC = new Vector2(99, 99);
+    Vector2 pointD = new Vector2(99, 99);
+
     // Start is called before the first frame update
     void Start() {
-
+        aAndBValue = GenerateAAndB();
+        Debug.Log("Debug 1: A: " + aAndBValue.x.ToString() + "B: " + aAndBValue.y.ToString());
+        linearFormula = BuildFormulaFromAAndB((int) aAndBValue.x, (int) aAndBValue.y);
+        Debug.Log("Debug 2: Formula: " + linearFormula);
+        pointA.x = XPointsOnGraph()[0];
+        Debug.Log("Debug 3: Point A: (" + pointA.x.ToString() + ", " + pointA.y.ToString() + ").");
+        pointB.x = XPointsOnGraph()[1];
+        Debug.Log("Debug 4: Point B: (" + pointB.x.ToString() + ", " + pointB.y.ToString() + ").");
+        pointC.x = XPointsOnGraph()[2];
+        Debug.Log("Debug 5: Point C: (" + pointC.x.ToString() + ", " + pointC.y.ToString() + ").");
+        pointD.x = XPointsOnGraph()[3];
+        Debug.Log("Debug 6: Point D: (" + pointD.x.ToString() + ", " + pointD.y.ToString() + ").");
     }
 
     // Update is called once per frame
@@ -37,11 +56,25 @@ public class LinearFormulaGenerator : MonoBehaviour {
         return returnValue;
     }
 
-    /*private int[] XPointsOnGraph() {
-        int[] xPoints;
+    private int[] XPointsOnGraph() {
+		int[] xPoints = new int[4] { 99, 99, 99, 99 };
 
+        foreach(int i in xPoints) {
+            xPoints[i] = Random.Range(minValue, maxValue);
+            Debug.Log("Nr. " + i.ToString() + " value: " + xPoints[i].ToString());
 
+            foreach(int j in xPoints) {
+                if(i != j) {
+                    while(xPoints[i] == xPoints[j]) {
+                        xPoints[i] = Random.Range(minValue, maxValue);
+                    }
+                }
+            }
+            Debug.Log("Na dubbel check. Nr. " + i.ToString() + " value: " + xPoints[i].ToString());
+        }
 
         return xPoints;
-    }*/
+    }
+
+    //Calc y from x
 }
