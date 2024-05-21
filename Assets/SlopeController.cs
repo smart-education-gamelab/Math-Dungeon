cce61cc2342f035b3fc801018ae8cb922456940e
ikarian7 <scott.marchant7@gmail.com>
2024-05-21 15:17:48 +0200

    test

---
+++ Assets/SlopeController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
+
+public class SlopeController : NetworkBehaviour
+{
+    public Slider slopeSlider;
+    public LineRenderer lineRenderer;
+
+    // Networked variable to sync the slope value
+    private NetworkVariable<float> networkedSlope = new NetworkVariable<float>(0.5f, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    private void Start()
+    {
+        if (IsOwner)
+        {
+            slopeSlider.onValueChanged.AddListener(OnSliderValueChanged);
+        }
+
+        // Initial update
+        UpdateLineRenderer(networkedSlope.Value);
+
+        // Listen for changes in the networked variable
+        networkedSlope.OnValueChanged += OnNetworkedSlopeChanged;
+    }
+
+    private void OnSliderValueChanged(float value)
+    {
+        if (IsOwner)
+        {
+            networkedSlope.Value = value;
+        }
+    }
+
+    private void OnNetworkedSlopeChanged(float oldValue, float newValue)
+    {
+        UpdateLineRenderer(newValue);
+    }
+
+    private void UpdateLineRenderer(float slope)
+    {
+        // Assuming the line starts at (0,0) and ends at (1, slope)
+        Vector3[] positions = new Vector3[2];
+        positions[0] = new Vector3(0, 0, 0);
+        positions[1] = new Vector3(1, slope, 0);
+        lineRenderer.SetPositions(positions);
+    }
+}
