7dc057e41f856b94a07f4ebf29673891f9de6ddf
ikarian7 <scott.marchant7@gmail.com>
2024-05-28 11:59:38 +0200

    Update PlayerActions.cs

--- Assets/Scripts/Player/PlayerActions.cs
+++ Assets/Scripts/Player/PlayerActions.cs
@@ -6,270 +6,269 @@ using Unity.Netcode;
 using Newtonsoft.Json;
 using System.Linq;
 
-public class PlayerActions : NetworkBehaviour{
+public class PlayerActions : NetworkBehaviour
+{
     private bool isNearActivationBall;
     private GameObject ballThatIsNear;
 
-	private bool isNearCauldron;
-	private GameObject cauldronCanvasA;
-	private GameObject cauldronCanvasB;
+    private bool isNearCauldron;
+    private GameObject cauldronCanvasA;
+    private GameObject cauldronCanvasB;
 
-	private bool isNearLever;
+    private bool isNearLever;
 
-	private bool isNearPipeLever;
-	private GameObject sliderCanvas;
+    private bool isNearPipeLever;
+    private GameObject sliderCanvas;
 
-	private GameObject potionControllerRef;
+    private GameObject potionControllerRef;
 
-	[SerializeField]
-	private GameObject movingWallA;
+    [SerializeField]
+    private GameObject movingWallA;
 
-	[SerializeField]
-	private GameObject movingWallB;
+    [SerializeField]
+    private GameObject movingWallB;
 
-	private string canvasName;
+    private string canvasName;
 
-	public bool IsNearActivationBall {
-		get => isNearActivationBall;
-		set => isNearActivationBall = value;
-	}
-	public GameObject BallThatIsNear {
-		get => ballThatIsNear;
-		set => ballThatIsNear = value;
-	}
-
-	public bool IsNearCauldron {
-		get => isNearCauldron;
-		set => isNearCauldron = value;
-	}
-	public string CanvasName{
-		get => canvasName;
-		set => canvasName = value;
-	}
-
-	public bool IsNearLever
+    public bool IsNearActivationBall
     {
-		get => isNearLever;
-		set => isNearLever = value;
+        get => isNearActivationBall;
+        set => isNearActivationBall = value;
+    }
+    public GameObject BallThatIsNear
+    {
+        get => ballThatIsNear;
+        set => ballThatIsNear = value;
     }
 
-	public bool IsNearPipeLever
-	{
-		get => isNearPipeLever;
-		set => isNearPipeLever = value;
-	}
-
-	public GameObject SliderCanvas
-	{
-		get => sliderCanvas;
-		set => sliderCanvas = value;
-	}
-
-	//Kamer A
-	public GameObject CauldronCanvasA {
-		get => cauldronCanvasA;
-		set => cauldronCanvasA = value;
-	}
-
-	//Kamer B
-	public GameObject CauldronCanvasB {
-		get => cauldronCanvasB;
-		set => cauldronCanvasB = value;
+    public bool IsNearCauldron
+    {
+        get => isNearCauldron;
+        set => isNearCauldron = value;
+    }
+    public string CanvasName
+    {
+        get => canvasName;
+        set => canvasName = value;
     }
 
-	//Kamer A
-	private string inputAnswerCXRoomA;
-	private string correctAnswerCXRoomA;
-	private string inputAnswerCYRoomA;
-	private string correctAnswerCYRoomA;
-
-	//Kamer B
-	private string inputAnswerFXRoomB;
-	private string correctAnswerFXRoomB;
-	private string inputAnswerFYRoomB;
-	private string correctAnswerFYRoomB;
-
-	private bool RoomACorrect;
-	private bool RoomBCorrect;
-
-	private Dictionary<ulong, ArrayList> clientAnswers = new Dictionary<ulong, ArrayList>();
-
-	// Start is called before the first frame update
-	void Start()
+    public bool IsNearLever
     {
-		//Kamer A
-		inputAnswerCXRoomA = "666";
-		correctAnswerCXRoomA = "667";
-		inputAnswerCYRoomA = "668";
-		correctAnswerCYRoomA = "669";
+        get => isNearLever;
+        set => isNearLever = value;
+    }
 
-		//Kamer B
-		inputAnswerFXRoomB = "670";
-		correctAnswerFXRoomB = "671";
-		inputAnswerFYRoomB = "672";
-		correctAnswerFYRoomB = "673";
+    public bool IsNearPipeLever
+    {
+        get => isNearPipeLever;
+        set => isNearPipeLever = value;
+    }
 
+    public GameObject SliderCanvas
+    {
+        get => sliderCanvas;
+        set => sliderCanvas = value;
+    }
 
-		IsNearActivationBall = false;
-		IsNearCauldron = false;
-		IsNearLever = false;
+    //Kamer A
+    public GameObject CauldronCanvasA
+    {
+        get => cauldronCanvasA;
+        set => cauldronCanvasA = value;
+    }
 
-		movingWallA = GameObject.Find("DoorA");
-		movingWallB = GameObject.Find("DoorB");
+    //Kamer B
+    public GameObject CauldronCanvasB
+    {
+        get => cauldronCanvasB;
+        set => cauldronCanvasB = value;
+    }
 
+    //Kamer A
+    private PointData inputAnswerCRoomA;
+    private PointData correctAnswerCRoomA;
 
-		RoomACorrect = false;
-		RoomBCorrect = false;
+    //Kamer B
+    private PointData inputAnswerFRoomB;
+    private PointData correctAnswerFRoomB;
+
+    private bool RoomACorrect;
+    private bool RoomBCorrect;
+
+    private Dictionary<ulong, PointData> clientAnswers = new Dictionary<ulong, PointData>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialize input and correct answers with placeholder values
+        inputAnswerCRoomA = new PointData { x = 666, y = 668 };
+        correctAnswerCRoomA = new PointData { x = 667, y = 669 };
+
+        inputAnswerFRoomB = new PointData { x = 670, y = 672 };
+        correctAnswerFRoomB = new PointData { x = 671, y = 673 };
+
+        IsNearActivationBall = false;
+        IsNearCauldron = false;
+        IsNearLever = false;
+
+        movingWallA = GameObject.Find("DoorA");
+        movingWallB = GameObject.Find("DoorB");
+
+        RoomACorrect = false;
+        RoomBCorrect = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-		potionControllerRef = GameObject.FindWithTag("PotionController");
-		if(Input.GetButtonDown("Activate")) {
-			if(IsNearActivationBall) {
-				BallThatIsNear.GetComponent<InteractableMechanism>().Activate();
-			} else if(IsNearCauldron) {
-				if (canvasName == "A") {
-					cauldronCanvasA.SetActive(!cauldronCanvasA.activeSelf);
-				} else if (canvasName == "B") {
-					cauldronCanvasB.SetActive(!cauldronCanvasB.activeSelf);
-                }
-
-				this.gameObject.GetComponent<PlayerMotor>().CanWalk = !this.gameObject.GetComponent<PlayerMotor>().CanWalk;
-				if(Cursor.lockState == CursorLockMode.Locked) {
-					Cursor.lockState = CursorLockMode.None;
-					Cursor.visible = true;
-                } else {
-					Cursor.lockState = CursorLockMode.Locked;
-					Cursor.visible = false;
-                }
-			} else if(IsNearLever){
-				//Kamer A
-				inputAnswerCXRoomA = FindChildWithTag(cauldronCanvasA, "InputAnswerXCTag").GetComponent<TMP_InputField>().text;
-				correctAnswerCXRoomA = potionControllerRef.GetComponent<LinearFormulaGeneratorSync>().AnswerXC.ToString();
-				inputAnswerCYRoomA = FindChildWithTag(cauldronCanvasA, "InputAnswerYCTag").GetComponent<TMP_InputField>().text;
-				correctAnswerCYRoomA = potionControllerRef.GetComponent<LinearFormulaGeneratorSync>().AnswerYC.ToString();
-
-				//Kamer B
-				inputAnswerFXRoomB = FindChildWithTag(cauldronCanvasB, "InputAnswerXFTag").GetComponent<TMP_InputField>().text;
-				correctAnswerFXRoomB = potionControllerRef.GetComponent<LinearFormulaGeneratorSync>().AnswerXF.ToString();
-				inputAnswerFYRoomB = FindChildWithTag(cauldronCanvasB, "InputAnswerYFTag").GetComponent<TMP_InputField>().text;
-				correctAnswerFYRoomB = potionControllerRef.GetComponent<LinearFormulaGeneratorSync>().AnswerYF.ToString();
-
-				//RequestAnswersServerRpc();
-				ArrayList jsonPayloadAnswersList = new ArrayList();
-				if (inputAnswerCXRoomA == "0" && inputAnswerCYRoomA == "0")
-				{
-					Debug.Log("Kamer B antwoord x: " + inputAnswerFXRoomB);
-					Debug.Log("Kamer B antwoord y: " + inputAnswerFYRoomB);
-					jsonPayloadAnswersList.Add(inputAnswerFXRoomB);
-					jsonPayloadAnswersList.Add(inputAnswerFYRoomB);
-				}
-				else
-				{
-					Debug.Log("Kamer A antwoord x: " + inputAnswerCXRoomA);
-					Debug.Log("Kamer A antwoord y: " + inputAnswerCYRoomA);
-					jsonPayloadAnswersList.Add(inputAnswerCXRoomA);
-					jsonPayloadAnswersList.Add(inputAnswerCYRoomA);
-				}
-				string jsonPayload = JsonConvert.SerializeObject(jsonPayloadAnswersList);
-				SendAnswersServerRpc(jsonPayload);
-			} else if(IsNearPipeLever)
+        potionControllerRef = GameObject.FindWithTag("PotionController");
+        if (Input.GetButtonDown("Activate"))
+        {
+            if (IsNearActivationBall)
             {
-				sliderCanvas.SetActive(!sliderCanvas.activeSelf);
+                BallThatIsNear.GetComponent<InteractableMechanism>().Activate();
+            }
+            else if (IsNearCauldron)
+            {
+                if (canvasName == "A")
+                {
+                    cauldronCanvasA.SetActive(!cauldronCanvasA.activeSelf);
+                }
+                else if (canvasName == "B")
+                {
+                    cauldronCanvasB.SetActive(!cauldronCanvasB.activeSelf);
+                }
 
-				this.gameObject.GetComponent<PlayerMotor>().CanWalk = !this.gameObject.GetComponent<PlayerMotor>().CanWalk;
+                this.gameObject.GetComponent<PlayerMotor>().CanWalk = !this.gameObject.GetComponent<PlayerMotor>().CanWalk;
+                if (Cursor.lockState == CursorLockMode.Locked)
+                {
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
+                }
+                else
+                {
+                    Cursor.lockState = CursorLockMode.Locked;
+                    Cursor.visible = false;
+                }
+            }
+            else if (IsNearLever)
+            {
+                // Kamer A
+                inputAnswerCRoomA = new PointData
+                {
+                    x = int.Parse(FindChildWithTag(cauldronCanvasA, "InputAnswerXCTag").GetComponent<TMP_InputField>().text),
+                    y = int.Parse(FindChildWithTag(cauldronCanvasA, "InputAnswerYCTag").GetComponent<TMP_InputField>().text)
+                };
+                correctAnswerCRoomA = new PointData
+                {
+                    x = potionControllerRef.GetComponent<LinearFormulaGeneratorSync>().AnswerXC,
+                    y = potionControllerRef.GetComponent<LinearFormulaGeneratorSync>().AnswerYC
+                };
 
-				if (Cursor.lockState == CursorLockMode.Locked)
-				{
-					Cursor.lockState = CursorLockMode.None;
-					Cursor.visible = true;
-				}
-				else
-				{
-					Cursor.lockState = CursorLockMode.Locked;
-					Cursor.visible = false;
-				}
-			}
-		}
-    }
+                // Kamer B
+                inputAnswerFRoomB = new PointData
+                {
+                    x = int.Parse(FindChildWithTag(cauldronCanvasB, "InputAnswerXFTag").GetComponent<TMP_InputField>().text),
+                    y = int.Parse(FindChildWithTag(cauldronCanvasB, "InputAnswerYFTag").GetComponent<TMP_InputField>().text)
+                };
+                correctAnswerFRoomB = new PointData
+                {
+                    x = potionControllerRef.GetComponent<LinearFormulaGeneratorSync>().AnswerXF,
+                    y = potionControllerRef.GetComponent<LinearFormulaGeneratorSync>().AnswerYF
+                };
 
-	[ServerRpc(RequireOwnership = false)]
-	private void SendAnswersServerRpc(string payload,  ServerRpcParams serverRpcParams = default)
-    {
-		ulong clientID = serverRpcParams.Receive.SenderClientId;
-		ArrayList answersFromClient = JsonConvert.DeserializeObject<ArrayList>(payload);
-		string inAnswX = JsonConvert.DeserializeObject<string>(answersFromClient[0].ToString());
-		string inAnswY = JsonConvert.DeserializeObject<string>(answersFromClient[1].ToString());
+                PointData answerToSend;
+                if (inputAnswerCRoomA.x == 0 && inputAnswerCRoomA.y == 0)
+                {
+                    Debug.Log("Kamer B antwoord x: " + inputAnswerFRoomB.x);
+                    Debug.Log("Kamer B antwoord y: " + inputAnswerFRoomB.y);
+                    answerToSend = inputAnswerFRoomB;
+                }
+                else
+                {
+                    Debug.Log("Kamer A antwoord x: " + inputAnswerCRoomA.x);
+                    Debug.Log("Kamer A antwoord y: " + inputAnswerCRoomA.y);
+                    answerToSend = inputAnswerCRoomA;
+                }
+                string jsonPayload = JsonConvert.SerializeObject(answerToSend);
+                SendAnswersServerRpc(jsonPayload);
+            }
+            else if (IsNearPipeLever)
+            {
+                sliderCanvas.SetActive(!sliderCanvas.activeSelf);
 
-		clientAnswers[clientID] = answersFromClient;
+                this.gameObject.GetComponent<PlayerMotor>().CanWalk = !this.gameObject.GetComponent<PlayerMotor>().CanWalk;
 
-		Debug.Log("Client ID: " + clientID);
-		Debug.Log("clients: " + clientAnswers.Keys.ToArray());
-		Debug.Log("aantal clients: " + clientAnswers.Keys.ToArray().Length);
-
-		/*if(clientAnswers.Keys.ToArray().Length < 2)
-        {
-			Debug.Log("nee");
-			return;
-        }*/
-
-		bool isItGood = false;
-
-		if (clientID == 1)
-		{
-			if (inAnswX == correctAnswerCXRoomA && inAnswY == correctAnswerCYRoomA)
-			{
-				isItGood = true;
-			}
-			else
-			{
-				isItGood = false;
-			}
-		} else
-        {
-			if (inAnswX == correctAnswerFXRoomB && inAnswY == correctAnswerFYRoomB)
-			{
-				isItGood = true;
-			}
-			else
-			{
-				isItGood = false;
-			}
-		}
-
-		handleSubmittedAnswersClientRpc(isItGood);
-	}
-
-	[ClientRpc]
-	private void handleSubmittedAnswersClientRpc(bool isGood)
-    {
-		if(isGood)
-        {
-			Debug.Log("Hoeraaaaaaaaaaaaaaaaaaaaaaaaaa BOTH");
-			movingWallA.GetComponent<InteractableMechanism>().Activate();
-			movingWallB.GetComponent<InteractableMechanism>().Activate();
-		} else
-        {
-
+                if (Cursor.lockState == CursorLockMode.Locked)
+                {
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
+                }
+                else
+                {
+                    Cursor.lockState = CursorLockMode.Locked;
+                    Cursor.visible = false;
+                }
+            }
         }
     }
 
-	GameObject FindChildWithTag(GameObject parent, string tag)
-	{
-		GameObject child = null;
+    [ServerRpc(RequireOwnership = false)]
+    private void SendAnswersServerRpc(string payload, ServerRpcParams serverRpcParams = default)
+    {
+        ulong clientID = serverRpcParams.Receive.SenderClientId;
+        PointData answersFromClient = JsonConvert.DeserializeObject<PointData>(payload);
 
-		foreach (Transform transform in parent.transform)
-		{
-			if (transform.CompareTag(tag))
-			{
-				child = transform.gameObject;
-				break;
-			}
-		}
+        clientAnswers[clientID] = answersFromClient;
 
-		return child;
-	}
+        Debug.Log("Client ID: " + clientID);
+        Debug.Log("clients: " + string.Join(", ", clientAnswers.Keys.ToArray()));
+        Debug.Log("aantal clients: " + clientAnswers.Count);
 
+        bool isItGood = false;
+
+        if (clientID == 1)
+        {
+            if (answersFromClient.x == correctAnswerCRoomA.x && answersFromClient.y == correctAnswerCRoomA.y)
+            {
+                isItGood = true;
+            }
+        }
+        else
+        {
+            if (answersFromClient.x == correctAnswerFRoomB.x && answersFromClient.y == correctAnswerFRoomB.y)
+            {
+                isItGood = true;
+            }
+        }
+
+        handleSubmittedAnswersClientRpc(isItGood);
+    }
+
+    [ClientRpc]
+    private void handleSubmittedAnswersClientRpc(bool isGood)
+    {
+        if (isGood)
+        {
+            Debug.Log("Hoeraaaaaaaaaaaaaaaaaaaaaaaaaa BOTH");
+            movingWallA.GetComponent<InteractableMechanism>().Activate();
+            movingWallB.GetComponent<InteractableMechanism>().Activate();
+        }
+        else
+        {
+            Debug.Log("Incorrect answers.");
+        }
+    }
+
+    GameObject FindChildWithTag(GameObject parent, string tag)
+    {
+        foreach (Transform transform in parent.transform)
+        {
+            if (transform.CompareTag(tag))
+            {
+                return transform.gameObject;
+            }
+        }
+        return null;
+    }
 }
